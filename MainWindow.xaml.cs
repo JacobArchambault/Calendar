f3f4d0fb3a73a11212a6e71ab0384ce96f94e881
Jacob Archambault <jacob.archambault@waystar.com>
2020-07-25 18:29:22 -0400

    Add warning label to main window and use it to give error text to reader.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,39 @@ namespace Calendar
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void AddEventButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (FieldsAreFilled(EventNameTextBox, StartTimeTextBox, EndTimeTextBox, LocationTextBox))
+            {
+                if (AreHours(StartTimeTextBox, EndTimeTextBox))
+                {
+                    WarningLabel.Content = "";
+                }
+                else
+                {
+                    WarningLabel.Content = "Invalid data. You must enter a time in format XX:XX in both hours sections";
+                }
+            }
+            else
+            {
+                WarningLabel.Content = "All non-optional fields must be filled";
+            }
+        }
+
+        private bool AreHours(params TextBox[] textboxes)
+        {
+            // The regex for matching a time string, credit Steve Valaitis.
+            string pattern = @"^((([0]?[1-9]|1[0-2])(:|\.)[0-5][0-9]((:|\.)[0-5][0-9])?( )?(AM|am|aM|Am|PM|pm|pM|Pm))|(([0]?[0-9]|1[0-9]|2[0-3])(:|\.)[0-5][0-9]((:|\.)[0-5][0-9])?))$";
+            // Check that all textboxes passed in as parameters match the 
+            return textboxes.All(t => Regex.IsMatch(t.Text, pattern));
+        }
+
+        // Returns true if no textboxes in the passed in textbox list are null or whitespace.
+        private bool FieldsAreFilled(params TextBox[] textBoxList)
+        {
+            return !((from t in textBoxList where string.IsNullOrWhiteSpace(t.Text) select t).Any());
         }
     }
 }
