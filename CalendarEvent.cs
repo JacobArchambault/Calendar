1440d6a9acd315f9abaa425a9837cc079d0c7b18
Jacob Archambault <jacob.archambault@waystar.com>
2020-07-25 12:34:28 -0400

    Add Name, StartTime, EndTime, Description and Location fields to CalendarEvent class.

--- CalendarEvent.cs
+++ CalendarEvent.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.DirectoryServices;
 using System.Text;
 
 namespace Calendar
 {
-    class CalendarEvent
+    internal class CalendarEvent
     {
+        internal string Name { get; set; }
+        internal DateTime StartTime { get; set; }
+        internal DateTime EndTime { get; set; }
+        internal string Location { get; set; }
+        internal string Description { get; set; }
+
     }
 }
