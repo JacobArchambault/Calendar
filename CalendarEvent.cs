f98e53fec84b250b2c78c26aaeb02fb473c00071
Jacob Archambault <jacob.archambault@waystar.com>
2020-07-25 13:32:48 -0400

    Use NodaTime in CalendarEvent instead of DateTime for start and end time properties.

--- CalendarEvent.cs
+++ CalendarEvent.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using System.DirectoryServices;
 using System.Text;
-
+using NodaTime;
 namespace Calendar
 {
     internal class CalendarEvent
     {
         internal string Name { get; set; }
-        internal DateTime StartTime { get; set; }
-        internal DateTime EndTime { get; set; }
+        internal LocalTime StartTime { get; set; }
+        internal LocalTime EndTime { get; set; }
         internal string Location { get; set; }
         internal string Description { get; set; }
 
