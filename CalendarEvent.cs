85d65c08fa3e6a055b8662a88fb9b9fdbc3f2c38
Jacob Archambault <jacob.archambault@waystar.com>
2020-07-25 16:08:36 -0400

    Add datetime to CalendarEvent object and associated label and datepicker to form.

--- CalendarEvent.cs
+++ CalendarEvent.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using System.DirectoryServices;
 using System.Text;
-using NodaTime;
 namespace Calendar
 {
     internal class CalendarEvent
     {
         internal string Name { get; set; }
         internal string StartTime { get; set; }
+        internal DateTime Date { get; set; }
         internal string EndTime { get; set; }
         internal string Location { get; set; }
         internal string Description { get; set; }
